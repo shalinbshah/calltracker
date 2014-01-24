a1d88b9804fcf6b04ea3ebe930ac121d6855b40e
shalinbshah <shalin.ldit11@gmail.com>
2014-01-24 23:30:40 +0530

    new holder

---
+++ src/com/call/tracker/contactmanager/TempHolder.java
@@ -0,0 +1,8 @@
+package com.call.tracker.contactmanager;
+
+import android.content.Intent;
+
+public class TempHolder {
+	public static Intent pickedContact = null;
+	public static String selectedGroup = "";
+}
